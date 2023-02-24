01485e943d7bad2d05d049e9205798d1c4fb26b7
nicholaskeng <63685215+nicholaskeng@users.noreply.github.com>
2023-02-23 22:49:34 -0500

    Create 1684.CounttheNumberofConsistentStrings.java

---
+++ 1684.CounttheNumberofConsistentStrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+
+        //boolean flag = false;
+
+        HashSet<Character> set = new HashSet<Character>();
+
+        for(char c: allowed.toCharArray())
+        {
+            set.add(c);
+        }
+
+        for(String s: words)
+        {
+            for(int i = 0; i < s.length(); i++)
+            {
+                if(!set.contains(s.charAt(i)))
+                {
+                    break;
+                }
+                if(i == s.length() - 1)
+                {
+                    count += 1;
+                }
+            }
+        }
+        
+        return count;
+    }
+}//contains
+//s1.contains("GFG")
